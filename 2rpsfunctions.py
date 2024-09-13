c31d6000419ee8f659f48bbb224f5985dfa3aeda
Axel-Almeida <almeidarubio07@gmail.com>
2024-09-13 10:50:17 -0600

    2 option

--- 2rpsfunctions.py
+++ 2rpsfunctions.py
@@ -8,54 +8,40 @@ value=True
 score=0
 program_score=0
 ties=0
-while(value):
-    selected = random.choice(word_list)
-    choice=input("Enter your choice in lowercase:")
-    def rots():
-        print("Its a tie!")
-        print("  ")
-        print("  ")
-        ties+=1
-    def win():
-            print("You Win")
-            print("  ")
-            print("  ")
-            score+=1
-    def loose():
-        print("You loose, I Win!!!!!!")
-        print(selected+" is what i chose")
-        print("  ")
-        print("  ")
-        program_score+=1
-    def stonks():
-        print("Here are the statistics")
-        print("   ")
-        print(f"Your score is {score}")
-        print(f"This is my score {program_score}")
-        print(f"These are ties or null points and the number is {ties}")
-        cont_game=input("Do you wanna play more (y/n)?")
-        if cont_game=="y"or "yes":
-            continue
-        else:
-            print("It was my pleasure serving you my lord")
-            print("   ")
-            print("   ")
-            break
-    def konga():
+def space():
+    print("  ")
+    print("  ")
+def rots():
+    print("Its a tie!")
+    space()
+    ties+=1
+def win():
+    print("You Win")
+    space()
+    score+=1
+def loose():
+    print("You loose, I Win!!!!!!")
+    print(selected+" is what i chose")
+    space()
+    program_score+=1
+def konga():
         if selected == 'paper':
             loose()
         else:
              win     
-    def troya():
-        if selected == 'rock':
-            loose()
-        else:
-            win()
-    def makemake():
-        if selected == 'scissors':
-            loose()
-        else:
-             win()
+def troya():
+    if selected == 'rock':
+        loose()
+    else:
+        win()
+def makemake():
+    if selected == 'scissors':
+        loose()
+    else:
+        win()
+while(value):
+    selected = random.choice(word_list)
+    choice=input("Enter your choice in lowercase:")
     if selected==choice:
         rots()
     elif choice == 'rock':
@@ -65,4 +51,15 @@ while(value):
     elif choice == 'scissors':
         troya()
     elif choice == "stacs":
-        stonks
+        print("Here are the statistics")
+        space()
+        print(f"Your score is {score}")
+        print(f"This is my score {program_score}")
+        print(f"These are ties or null points and the number is {ties}")
+        cont_game=input("Do you wanna play more (y/n)?")
+        if cont_game=="y"or "yes":
+            continue
+        else:
+            print("It was my pleasure serving you my lord")
+            space()
+            break
