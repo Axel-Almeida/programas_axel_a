57e1f359d659723d4f8c1914d79b1e5405e91d3b
Axel-Almeida <almeidarubio07@gmail.com>
2024-10-17 10:39:40 -0600

    2 option

---
+++ test_weather.py
@@ -0,0 +1,19 @@
+# test_weather.py
+import pytest
+from pytest import approx
+from weather import cels_from_fahr
+
+
+
+ 
+
+def test_cels_from_fahr():
+    assert cels_from_fahr(-25) == approx(-31.66667)
+    assert cels_from_fahr(0) == approx(-17.77778)
+    assert cels_from_fahr(32) == approx(0)
+    assert cels_from_fahr(70) == approx(21.1111)
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
+
+
---
+++ weather.py
@@ -0,0 +1,9 @@
+def cels_from_fahr(fahr):
+    #Convert a temperature in Fahrenheit to
+    #Celsius and return the Celsius temperature.
+    cels = (fahr - 32) * 5 / 9
+    return cels
+
+print(cels_from_fahr(-5))
+print(cels_from_fahr(0))
+print(cels_from_fahr(37))
