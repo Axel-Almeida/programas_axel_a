9453c042f893669f227bcc9ea69def6ec0d8fcc1
Axel-Almeida <almeidarubio07@gmail.com>
2024-10-30 13:53:42 -0600

    2 option

--- 3term/dictionary.py
+++ 3term/dictionary.py
@@ -9,25 +9,25 @@ def local_contacts(filename):
             last_name=row[1]
             phone_number=row[2]
             emailaddres=row[3]
-            contacts[last_name]= [first_name, phone_number, emailaddres]
+            contacts[first_name]= [last_name, phone_number, emailaddres]
     return contacts
 
 def display_contact_info(contact_info):
     if contact_info:
         print("Contact Information:\n")
-        print(f"First Name: {contact_info[0]}")
+        print(f"Last Name: {contact_info[0]}")
         print(f"Phone NUmber: {contact_info[1]}")
         print(f"Eamil Addres: {contact_info[2]}")
     else:
         print("No contact information for this last name.")
 
 def main():
-    filename='CSV.csv'
+    filename='3term\CSV.csv'
 
     contacts=local_contacts(filename)
 
-    last_name= input("Please enter the last name to look up:").strip()
-    contact_info= contacts.get(last_name)
+    first_name= input("Please enter the last name to look up:").strip()
+    contact_info= contacts.get(first_name)
 
     display_contact_info(contact_info)
 
